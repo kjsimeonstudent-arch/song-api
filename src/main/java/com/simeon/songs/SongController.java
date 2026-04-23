fc51a84d10942e0c9d6df5853730e05ed906be12
Kenzhin <kjsimeon.student@ua.edu.ph>
2026-04-23 11:36:23 +0800

    Update Cross Origin V6

--- src/main/java/com/simeon/songs/SongController.java
+++ src/main/java/com/simeon/songs/SongController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "https://song-ui-d5fq.onrender.com")
 @RestController
 @RequestMapping("/simeon/songs")
 public class SongController {
--- src/main/java/com/simeon/songs/WebConfig.java
+++ src/main/java/com/simeon/songs/WebConfig.java
@@ -10,15 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Value("${ALLOWED_ORIGINS:https://song-ui-d5fq.onrender.com}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            @Value("${ALLOWED_ORIGINS")
-            private String allowedOrigins;
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all endpoints
-                        .allowedOrigins(allowedOrigins.split("https://song-ui-d5fq.onrender.com,")) // Support multiple origins separated by commas
+                        .allowedOrigins(allowedOrigins.split(",")) // Support multiple origins separated by commas
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // If using cookies or authentication headers
